eac56b9d21dd24b2c54f31a975c8c84947dd46d7
hayrapetyansami <samihayrapetyan@gmail.com>
2022-08-22 20:22:39 +0400

    added some functions and fixed some bugs

--- film/js/main.js
+++ film/js/main.js
@@ -1,13 +1,19 @@
 "use strict";
 const poster = document.querySelector("header img");
-const adv = document.querySelectorAll("#main_promo .mp");
 const filmsBlock = document.querySelector("#films");
 const form = document.querySelector("#add");
 
-poster.src = "img/bg2.jpg";
-poster.alt = "Hitman's Wife's Bodyguard";
-adv.forEach(adv => adv.remove());
-document.title = poster.alt;
+function init (x) {
+	poster.src = `img/bg${x}.jpg`;
+	if (poster.src.slice(-7) === "bg2.jpg") {
+		poster.alt = "Hitman's Wife's Bodyguard";
+	} else if (poster.src.slice(-7) === "bg1.jpg") {
+		poster.alt = "Gemini Man";
+	}
+	document.title = poster.alt;
+	document.querySelectorAll("#main_promo .mp").forEach(adv => adv.remove());
+}
+init(2);
 
 const _DB = {
 	movies: [
@@ -20,19 +26,14 @@ const _DB = {
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	let val = e.target.firstElementChild.value.trim();
-	let valCopy = val;
 	const check = document.querySelector("input[name='favorite']");
 
 	if (val !== "") {
-		if (val.length >= 21) {
-			val = `${val.slice(0, 21)}...`;
-		}
-
 		if (check.checked) {
-			console.log(`This film <${valCopy}> added to favorite`);
+			console.log(`This film <${val}> added to favorite`);
 		}
 
-		_DB.movies.push(valCopy);
+		_DB.movies.push(val);
 	}
 
 	setSort(_DB.movies);
@@ -51,7 +52,7 @@ function createFilmsList (films, parent) {
 	films.forEach((film, index) => {
 		parent.innerHTML += `
 			<p>
-				${index + 1}. ${film} 
+				${index + 1}. ${film.length >= 21 ? film.slice(0, 21)+'...' : film} 
 				<span data-rm>&#128465</span>
 			</p>
 		`;
