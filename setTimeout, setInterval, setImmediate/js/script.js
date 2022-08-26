92491ffd15dfd35daefe0daf67f2ee008d4e9e3d
hayrapetyansami <samihayrapetyan@gmail.com>
2022-08-26 21:35:17 +0400

    setInterval / clearInterval + setTimeout / clearTimeout

---
+++ setTimeout, setInterval, setImmediate/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+// let state = false;
+// const first = setTimeout(() => {
+// 	console.log(10 + 15);
+// 	state = true;
+// }, 1500);
+
+// if (state) {
+// 	clearTimeout(first); // ինչ-որ պայմանով
+// }
+
+// let i = 0;
+// const second = setInterval(() => {
+// 	i++;
+// 	console.log(i);
+
+// 	if (i === 500) {
+// 		clearInterval(second);
+// 	}
+// }, 0);
+
+// console.log("Hello");
+
+setTimeout(() => {
+	console.log(1);
+}, 0);
+
+console.log(2);
+console.log(3);
+console.log(4);
